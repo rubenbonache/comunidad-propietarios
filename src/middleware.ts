7d273f72ddab3b345d766f2bfe334c129e21cd08
Ruben <ruben.bonache@gmail.com>
2023-09-04 12:51:17 +0100

    Cambios autentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,15 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard"] }
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    console.log(req.nextauth.token)
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token?.role === "admin",
+    },
+  }
+)
+
+export const config = { matcher: ["/admin"] }
